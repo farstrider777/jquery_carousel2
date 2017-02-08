3e56d6890d736cd50ce4a9dbab93f2825b31a0d7
Joshua Bradford <jjyet@yahoo.com>
2017-02-08 15:30:18 -0500

    added arrows and made clicks work left and right

--- javascript/main.js
+++ javascript/main.js
@@ -1,2 +1,29 @@
-console.log(carouselImages[0].image_url)
+
 $("div").html(`<img src="${carouselImages[0].image_url}"/>`)
+$("div").after(`<img class=left src="images/left_arrow.png"/>`)
+$("div").after(`<img class=right src="images/left_arrow.png"/>`)
+
+var index = 0;
+
+$(".left").click(moveSlideLeft);
+$(".right").click(moveSlideRight);
+
+function moveSlideLeft(){
+  if(index === 0){
+    index = 4;
+  }else{
+  index--;
+  }
+  $("div").html(`<img src="${carouselImages[index].image_url}"/>`)
+}
+
+
+
+function moveSlideRight(){
+  if(index === 4){
+    index = 0;
+  }else{
+  index++;
+  }
+  $("div").html(`<img src="${carouselImages[index].image_url}"/>`)
+}
