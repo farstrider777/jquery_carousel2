7ea7be45362c9b739a4b24bd8180a5069301a697
Joshua Bradford <jjyet@yahoo.com>
2017-02-08 19:43:12 -0500

    added white words on bottom

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,6 @@
 
-$("div").html(`<img src="${carouselImages[0].image_url}"/>`)
+$("div").html(`<img src="${carouselImages[0].image_url}"/>
+<footer>"${carouselImages[0].title}" by <u>${carouselImages[0].photographer_id}</u>.</footer>`)
 $("div").after(`<img class="left low-opacity" src="images/left_arrow.png"/>`)
 $("div").after(`<img class="right low-opacity" src="images/left_arrow.png"/>`)
 
@@ -9,40 +10,16 @@ $(".left").click(moveSlideLeft);
 $(".right").click(moveSlideRight);
 
 function moveSlideLeft(){
+  //$("div").html(`<img src="${carouselImages[0].image_url}"/>
+  //<footer>"${carouselImages[0].title}" by <u>${carouselImages[0].photographer_id}</u>.</footer>`)
   if(index === 0){
     index = 4;
   }else{
   index--;
   }
-  $("div").html(`<img src="${carouselImages[index].image_url}"/>`)
+  $("div").html(`<img src="${carouselImages[index].image_url}"/>
+  <footer>"${carouselImages[index].title}" by <u>${carouselImages[index].photographer_id}</u>.</footer>`)
 }
-
-/*
-$(".left").on("mouseover", lDarkenArrow);
-
-function lDarkenArrow(){
-  console.log("darken")
-  $(".left").removeClass("low-opacity").addClass("high-opacity");
-}
-*/
-
-
-
-$(".left").hover(function() {
-  console.log("entered");
-  $(".left").removeClass("low-opacity");
-}, function() {
-  $(".left").addClass("low-opacity");
-});
-
-$(".right").hover(function() {
-  console.log("entered");
-  $(".right").removeClass("low-opacity");
-}, function() {
-  $(".right").addClass("low-opacity");
-});
-
-
 
 function moveSlideRight(){
   if(index === 4){
@@ -50,5 +27,19 @@ function moveSlideRight(){
   }else{
   index++;
   }
-  $("div").html(`<img src="${carouselImages[index].image_url}"/>`)
+  $("div").html(`<img src="${carouselImages[index].image_url}"/>
+  <footer>"${carouselImages[index].title}" by <u>${carouselImages[index].photographer_id}</u>.</footer>`)
 }
+
+
+$(".left").hover(function() {
+  $(".left").removeClass("low-opacity");
+}, function() {
+  $(".left").addClass("low-opacity");
+});
+
+$(".right").hover(function() {
+  $(".right").removeClass("low-opacity");
+}, function() {
+  $(".right").addClass("low-opacity");
+});
